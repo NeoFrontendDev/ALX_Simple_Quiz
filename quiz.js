d4499b860df7631923b4bfdbaa39df16eb227e6b
Neo Mosebi <neosammosebi01@gmail.com>
2025-06-14 00:29:55 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,12 @@
-function checkAnswer() {
-  correctAnswer = "4";
+function checkAnswer(userAnswer) {
+    const correctAnswer = "4";
+    const selectedOption = document.querySelector('input[name="quiz"]:checked')
+    
+    if (userAnswer === correctAnswer) {
+        console.log("Correct! Well done.");
+    } else {
+        console.log("That's incorrect. Try again!");
+    }
 }
+
+document.getElementById("submit-answer");
